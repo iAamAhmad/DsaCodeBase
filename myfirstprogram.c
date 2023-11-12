5d1995cc2cf493d559a8e259869dd8e5916522bf
iAamAhmad <aghazi1011@gmail.com>
2023-11-12 11:38:45 +0500

    DSA-one

---
+++ myfirstprogram.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+
+// int main() {
+//   printf("Hello World! \n");
+//   printf("Hello World! \n");
+//   printf("Hello World! \n");
+//   printf("Hello World!");
+//   return 0;
+// }
+
+#include <stdio.h>
+int main(){
+   int n;
+   
+
+   for(int i=0; i<10; i++){
+      printf("%d\n", A[i]);
+   }
+   return 0;
+}
