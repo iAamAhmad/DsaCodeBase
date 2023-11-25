952eabd0362d2226b4bdd89915a2f55e3e9d3964
iAamAhmad <aghazi1011@gmail.com>
2023-11-25 15:16:53 +0500

    stock market

--- 7.py
+++ 7.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1 # left = buy, riht = sell
+        l, r = 0, 1 # left = buy, riht = sell kkkk
         maxP = 0
         while l < len(prices) :
             if prices[l] < prices[r]:
