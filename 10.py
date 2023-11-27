4e1714a114d52b855128e2f58de577fa0ab35890
iAamAhmad <aghazi1011@gmail.com>
2023-11-27 13:17:21 +0500

    array

--- 10.py
+++ 10.py
@@ -1,1 +1,12 @@
 # Hello we are solving this question 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        currSum = 0
+        for n in nums :
+            if currSum < 0:
+                currSum = 0
+            currSum +=n
+            maxSum = max(maxSum,currSum)
+        return maxSum
+        
